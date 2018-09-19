c2958163305c5670f0aa4f2f56a243adf0a36318
Sergey Smolin <v-sesmol@microsoft.com>
2018-09-19 15:10:19 +0500

    Initial Commit

---
+++ SnowMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowMan
+//
+//  Created by Sergey Smolin on 19/09/2018.
+//  Copyright © 2018 Sergey Smolin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
